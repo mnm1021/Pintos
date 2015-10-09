4d316e064660930fe4da158b5c49100ada2b854d
YS <jin@ubuntu.(none)>
2015-10-09 12:42:26 +0900

    Assignment 8 : Priority Scheduling & Synchronization - 13/27 Passed

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,9 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      //list_push_back (&sema->waiters, &thread_current ()->elem);
+			/* Assignment 8 : Priority Synchronization */
+			list_insert_ordered( &sema->waiters, &thread_current()->elem, cmp_priority, NULL );
       thread_block ();
     }
   sema->value--;
@@ -113,10 +115,19 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
+  if (!list_empty (&sema->waiters))
+	{
+		/* Assignment 8 : sort list */
+		list_sort( &sema->waiters, cmp_priority, NULL );
+
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
+	}
   sema->value++;
+
+	/* Assignment 8 : Pre-emption */
+	test_max_priority();
+
   intr_set_level (old_level);
 }
 
@@ -295,7 +306,9 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+	/* Assignment 8 : Priority Scheduling */
+	list_insert_ordered( &cond->waiters, &waiter.elem, cmp_sem_priority, NULL );
+
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -316,9 +329,14 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
+  if (!list_empty (&cond->waiters))
+	{
+		/* Assignment 8 : sort list */
+		list_sort( &cond->waiters, cmp_sem_priority, NULL );
+
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+	}
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
@@ -335,4 +353,23 @@ cond_broadcast (struct condition *cond, struct lock *lock)
 
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
+}
+
+/*
+ * Assignment 8 : for list sorting
+ * compare the first element of semaphore's waiter thread.
+ */
+bool
+cmp_sem_priority (const struct list_elem *a, const struct list_elem *b, void *aux UNUSED)
+{
+	struct semaphore_elem *sa = list_entry( a, struct semaphore_elem, elem );
+	struct semaphore_elem *sb = list_entry( b, struct semaphore_elem, elem );
+
+	/* get thread descriptor */
+	struct thread *ta = list_entry( list_begin( &sa->semaphore.waiters ),
+	                                struct thread, elem );
+	struct thread *tb = list_entry( list_begin( &sb->semaphore.waiters ),
+	                                struct thread, elem );
+
+	return ta->priority > tb->priority;
 }
